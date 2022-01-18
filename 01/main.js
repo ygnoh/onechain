d87fed46c9fd4fe9d081dd73d8bb52a485806315
yonggoo-noh <yonggoo.noh@navercorp.com>
2022-01-18 21:59:57 +0900

    Calculate hash using crypto-js

--- 01/main.js
+++ 01/main.js
@@ -1,3 +1,15 @@
+const CryptoJS = require("crypto-js");
+
+function calculateHash(version, index, previousHash, timestamp, merkleRoot) {
+    return CryptoJS.SHA256(version + index + previousHash + timestamp + merkleRoot).toString().toUpperCase();
+}
+
+function calculateHashForBlock(block) {
+    const {version, index, previousHash, timestamp, merkleRoot} = block.header;
+
+    return calculateHash(version, index, previousHash, timestamp, merkleRoot);
+}
+
 class BlockHeader {
     constructor(version, index, previousHash, timestamp, merkleRoot) {
         this.version = version;
